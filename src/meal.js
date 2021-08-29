5d8b6f37908223b55a28a7a3354f24a664bf8d7d
Carly Collums <carlycollums@gmail.com>
2021-08-29 17:18:54 -0600

    Add meal test implementatiion

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,50 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`;
 
+}
+
+
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type,
+  };
+  return menuItem;
+}
+
+function addIngredients(toppings, ingredients) {
+  if (ingredients.includes(toppings) === false) {
+    return ingredients.push(toppings)
+  };
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+  return `$${formattedPrice}`;
+}
+
+
+function decreasePrice(decreasedPrice) {
+  return decreasedPrice * .9
+
+}
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  };
+  return recipe;
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
