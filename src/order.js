0ff1c1c9be29a0896db6ba662f7ba7fa2800e1f7
Carly Collums <carlycollums@gmail.com>
2021-08-29 17:19:20 -0600

    Add order test implementation

--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
+function takeOrder(orders, deliveryOrders) {
+   if (deliveryOrders.length < 3) {
+   return deliveryOrders.push(orders);
+  }
+}
+// deliveryOrders.length = 3
+// deliveryOrders.push(orders);
+
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+  if (deliveryOrders[i].orderNumber === orderNumber) {
+    deliveryOrders.splice(i, 1);
+    }
+  }
+}
+
+
+function listItems(deliveryOrders) {
+ var items = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    items.push(deliveryOrders[i].item);
+}
+return items.join(', ')
+}
+
+function searchOrder(deliveryOrders, list) {
+  var isListed = false;
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === list) {
+    isListed = true;
+    }
+  }
+  return isListed;
+}
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder,
 }
